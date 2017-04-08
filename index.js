7e7870835b63a166f27bcb1e5f563f81faf6e89a
Robertas Sadovskis <robert.sadovskis@gmail.com>
2017-04-08 12:38:41 +0300

    Ceec

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+	if (req.query['hub.verify_token'] === '12345') {
 		res.send(req.query['hub.challenge'])
 	} else {
 		res.send('Error, wrong token')
