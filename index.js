d87f34e1ce595bb5b77b94183ad4ef1d8ef6867d
Robertas Sadovskis <robert.sadovskis@gmail.com>
2017-04-08 13:11:51 +0300

    dew

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('1234567');
+    res.send('233410515');
 });
 
 // Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === '1503689886') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === '233410515') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
 });
